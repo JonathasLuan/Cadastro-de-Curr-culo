e687b98156a5e9ad904132058624e657e95d2ce8
Jonathas Luan <jonathas.araujo3@etec.sp.gov.br>
2022-11-04 19:45:53 -0300

    segundo commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro_de_Currículo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string nome;
+        string sobrenome;
+        string endereço;
+        string e_mail;
+        int telefone;
+        int data_de_nascimento;
+        double pretensão_salarial;
+
+        Console.WriteLine("digite seu nome:");
+        Console.ReadLine();
+        Console.WriteLine("digite seu sobrenome:");
+        Console.ReadLine();
+        Console.WriteLine("digite seu endereço:");
+        Console.ReadLine();
+        Console.WriteLine("digite seu e-mail");
+        Console.ReadLine();
+        Console.WriteLine("digite sua data de nascimento:");
+        Console.ReadLine();
+        Console.WriteLine("digite sua pretensão salarial:");
+
+
+        }
+    }
+}
